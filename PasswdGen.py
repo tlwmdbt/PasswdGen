e7092fa5794ced717c14e14673d960e3aac90388
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-22 20:54:02 +0200

    Update PasswdGen.py
    User Input möglich, mit abfangen von grobem Unfug.

--- PasswdGen.py
+++ PasswdGen.py
@@ -2,33 +2,25 @@
 #================================
 from random import *
 #================================
-
 while True:
-  Eingabe = input ('Wieviele Zeichen soll das Passwort habe: ')  
-  
+  Eingabe = input("Wieviele Stellen soll das Passwort haben: ")
   try:
-    Eingabe = int(Eingabe)
+      int_Eingabe = int(Eingabe)
+      if int_Eingabe <= 0 or int_Eingabe > 32:
+          print('Bitte nur positive Zahlen kleiner 32 eingeben!')
+          continue
+      break
   except ValueError:
-    # Eingabe ist nicht vom Typ Integer
-    print('Bitte nur Zahlen eingeben!')
-    break
+      print("Haha, witzig, das ist keine Zahl!")
   else:
-    # Eingabe ist vom Typ Integer
-    
-    if Eingabe <= 24:
-      tmp_passwd = []
-      i = 0
-      
-      for i in range(0, Eingabe):
-        element = choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.:,;#+*!\§$%&/()=?~ßöäüÖÄÜ')
-        
-        if element not in tmp_passwd:
-          tmp_passwd.append(element)
-          i = i + 1
-      
-      password = "".join(tmp_passwd)
-      print(password)
-    
-    else:
-      print("Es sind bis zu 24 Zeichen möglich.")
-    break
+    print("Was auch immer du jetzt wieder eingegeben hast das war keine gülltige Eingabe!")
+
+tmp_passwd = []
+i = 0
+
+for i in range(0, int_Eingabe):
+  element = choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.:,;#+*!\§$%&/()=?~ßöäüÖÄÜ')
+  tmp_passwd.append(element)
+  password = "".join(tmp_passwd)  
+  i = i + 1  
+print(password)
