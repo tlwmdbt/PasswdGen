c44ec67da78ec4bfffff426afe61c5a62a932434
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-23 18:08:27 +0200

    Update PasswdGen.py
    exception handling für den Fall eines nicht verfügbaren /dev/urandom hinzugefügt.

--- PasswdGen.py
+++ PasswdGen.py
@@ -1,6 +1,6 @@
 # Password Generator
 #================================
-from random import *
+from random import choice, seed, randint
 #================================
 
 # User Input aquired via while loop and exception handling
@@ -22,8 +22,24 @@ while True:
     # catch all for all other cases ¯\_(ツ)_/¯ 
     print("Höma, red ich chinesisch? 你这笨蛋！Eine Zahl zwischen 12 und 32. Los, nochma!")
 
-# Initialize the random generator with 
-seed(randint(1, 1000))
+# Initialize the random generator from OS specific random device
+while True:
+  try:
+    # check if urandom is working
+    # 
+    from os import urandom
+    seed(int.from_bytes(urandom(100), byteorder='big', signed=False))
+    print("Benutze /dev/urandom.")
+    break
+  except ImportError:
+    # module not available, so use standard randint
+    print("Benutze standard randint().")
+    seed(randint(1, 1000))
+    break
+  else:
+    # all other cases, bye ¯\_(ツ)_/¯ 
+    print("Kein random möglich.")
+    raise SystemExit
 
 # nescessary temporary list and a counter
 tmp_passwd = []
@@ -32,7 +48,7 @@ i = 0
 # while loop to create and choose from a alpha numeric and special character set
 for i in range(0, int_Eingabe):
   # setup the set of characters to choose from randomly
-  element = choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.:,;#+*!\§$%&/()=?~ßöäüÖÄÜ')
+  element = choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.:,;#+*!§$%&/()=?~ßöäüÖÄÜ')
   # append it to the temporary list tmp_passwd
   tmp_passwd.append(element)
   # count +1
@@ -41,3 +57,4 @@ for i in range(0, int_Eingabe):
 # construct the password from the list entries and print it to the screen
 password = "".join(tmp_passwd)  
 print("\n", password, "\n")
+raise SystemExit
